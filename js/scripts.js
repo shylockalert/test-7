3463c2ff06332019b7c6e3ac581130992b0137ff
shylockalert <sadiaafrindazai@gmail.com>
2024-08-16 15:33:23 +0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-/*!
-* Start Bootstrap - Blog Home v5.0.9 (https://startbootstrap.com/template/blog-home)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// script.js
+function toggleReadMore() {
+    const moreText = document.querySelector(".more-text");
+    const readMoreBtn = document.querySelector(".read-more-btn");
+
+    if (moreText.style.display === "none" || moreText.style.display === "") {
+        moreText.style.display = "inline";
+        readMoreBtn.textContent = "Read Less";
+    } else {
+        moreText.style.display = "none";
+        readMoreBtn.textContent = "Read More";
+    }
+}
